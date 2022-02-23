7f1ae8d39b7fe64c79fdfd64fbab88fea87c18c8
Alexis Fossart <alexis.fossart@cosmotech.com>
2022-02-23 17:41:39 +0100

    Defined base structure of the library

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='CosmoTech_Acceleration_Library',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    url='https://github.com/Cosmo-Tech/CosmoTech-Acceleration-Library',
+    license='MIT',
+    author='afossart',
+    author_email='alexis.fossart@cosmotech.com',
+    description='  Acceleration library for CosmoTech cloud based solution development'
+)
