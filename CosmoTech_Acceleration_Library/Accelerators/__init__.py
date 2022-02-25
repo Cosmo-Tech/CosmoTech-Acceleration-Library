269cc65ec559acc172b121892db7b37476dfb70b
Alexis Fossart <alexis.fossart@cosmotech.com>
2022-02-25 08:53:02 +0100

    Defined first acceleration function : apply_simple_csv_parameter_to_simulator
    This function allows to apply a csv parameter of format entity,value to a csm simulator

---
+++ CosmoTech_Acceleration_Library/Accelerators/__init__.py
@@ -0,0 +1,2 @@
+import os
+parametersPath = os.environ.get("CSM_PARAMETERS_ABSOLUTE_PATH")
--- CosmoTech_Acceleration_Library/Accelerators/csm_engine.py
+++ CosmoTech_Acceleration_Library/Accelerators/csm_engine.py
@@ -1,2 +1,26 @@
-def test_function():
-    print("Test")
+import os
+import csv
+import glob
+import json
+from . import parametersPath
+
+
+def apply_simple_csv_parameter_to_simulator(simulator,
+                                            parameter_name: str,
+                                            target_attribute_name: str,
+                                            csv_id_column: str = "id",
+                                            csv_value_column: str = "value"):
+    parameter_path = os.path.join(parametersPath, parameter_name)
+    if os.path.exists(parameter_path):
+        csv_files = glob.glob(os.path.join(parameter_path, "*.csv"))
+        for csv_filename in csv_files:
+            model = simulator.GetModel()
+            with open(csv_filename, "r") as csv_file:
+                for row in csv.DictReader(csv_file):
+                    entity_name = row.get(csv_id_column)
+                    value = json.loads(row.get(csv_value_column))
+                    entity = model.FindEntityByName(entity_name)
+                    if entity:
+                        entity.SetAttributeAsString(target_attribute_name, json.dumps(value))
+    else:
+        raise ValueError(f"Parameter {parameter_name} does not exists.")
