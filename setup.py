a70ccd0b1ad3ee03eeefbaa50d7af3c51f81c108
Vincent Carluer <vcarluer@gmail.com>
2022-03-01 17:57:20 +0100

    publish pypi action

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@
 # Licensed under the MIT license.
 import setuptools
 
+VERSION = "0.0.1"
+
 setuptools.setup(
     name='CosmoTech_Acceleration_Library',
-    version='0.0.1',
+    version=VERSION,
     packages=setuptools.find_packages(),
     url='https://github.com/Cosmo-Tech/CosmoTech-Acceleration-Library',
     license='MIT',
     author='afossart',
     author_email='alexis.fossart@cosmotech.com',
-    description='  Acceleration library for CosmoTech cloud based solution development',
+    description='Acceleration library for CosmoTech cloud based solution development',
     install_requires=[
         'azure-identity',
         'cosmotech_api @ git+https://github.com/Cosmo-Tech/cosmotech-api-python-client.git#egg=cosmotech_api',
