33f155b5f6d8f34e28b20d3c9c7886d0775652d5
jreynard <jeremy.reynard@cosmotech.com>
2022-07-07 10:51:21 +0200

    Add metrics in debug mode about export data time

--- CosmoTech_Acceleration_Library/Modelops/core/io/model_exporter.py
+++ CosmoTech_Acceleration_Library/Modelops/core/io/model_exporter.py
@@ -1,6 +1,7 @@
 # Copyright (c) Cosmo Tech corporation.
 # Licensed under the MIT license.
 import logging
+import time
 
 from CosmoTech_Acceleration_Library.Modelops.core.common.graph_handler import ExportableGraphHandler
 from CosmoTech_Acceleration_Library.Modelops.core.common.writer.CsvWriter import CsvWriter
@@ -26,12 +27,24 @@ class ModelExporter(ExportableGraphHandler):
         :return: Csv files containing all twin instances exported into {export_dir} folder named by twin type
         """
         logger.debug("Start exporting twins...")
+        logger.debug("Get twin types...")
+        get_types_start = time.time()
         twin_names = self.mr.get_twin_types()
+        get_types_end = time.time() - get_types_start
+        logger.debug(f"Get twin types took {get_types_end} s")
 
         for twin_name in twin_names:
+            logger.debug(f"Get twin info for type {twin_name} ...")
+            get_twin_info_start = time.time()
             headers = self.mr.get_twin_properties_by_type(twin_name)
             twin_results = self.mr.get_twins_by_type(twin_name)
+            get_twin_info_end = time.time() - get_twin_info_start
+            logger.debug(f"Get twin info for type {twin_name} took {get_twin_info_end} s")
+            logger.debug(f"Export twin info for type {twin_name} ...")
+            export_twin_info_start = time.time()
             CsvWriter.write_twin_data(self.export_dir, twin_name, twin_results, headers)
+            export_twin_info_end = time.time() - export_twin_info_start
+            logger.debug(f"Export twin info for type {twin_name} took {export_twin_info_end} s")
             logger.debug(f"Twins exported :{twin_name}")
         logger.debug("... End exporting twins")
 
@@ -43,12 +56,24 @@ class ModelExporter(ExportableGraphHandler):
         folder named by relationship type
         """
         logger.debug("Start exporting relationships...")
+        logger.debug("Get relationship types...")
+        get_relationship_types_start = time.time()
         relationship_names = self.mr.get_relationship_types()
+        get_relationship_types_end = time.time() - get_relationship_types_start
+        logger.debug(f"Get relationship types took {get_relationship_types_end} s")
 
         for relationship_name in relationship_names:
+            logger.debug(f"Get relationship info for type {relationship_name} ...")
+            get_relationship_info_start = time.time()
             headers = self.mr.get_relationship_properties_by_type(relationship_name)
             relationship_result = self.mr.get_relationships_by_type(relationship_name)
+            get_relationship_info_end = time.time() - get_relationship_info_start
+            logger.debug(f"Get relationship info for type {relationship_name} took {get_relationship_info_end} s")
+            logger.debug(f"Export relationship info for type {relationship_name} ...")
+            export_relationship_info_start = time.time()
             CsvWriter.write_relationship_data(self.export_dir, relationship_name, relationship_result, headers)
+            export_relationship_info_end = time.time() - export_relationship_info_start
+            logger.debug(f"Export relationship info for type {relationship_name} took {export_relationship_info_end} s")
             logger.debug(f"Relationships exported :{relationship_name}")
         logger.debug("... End exporting relationships")
 
