9adbe86b94ad22a03482585bbc651c26e5b4be58
Laurent Alépée <laurent.alepee@cosmotech.com>
2022-08-29 14:59:46 +0200

    move overrides and scripts for mkdocs to docs folder

--- docs/scripts/generate_index.py
+++ docs/scripts/generate_index.py
@@ -4,7 +4,7 @@ import mkdocs_gen_files
 import CosmoTech_Acceleration_Library
 
 _md_file: IO
-with mkdocs_gen_files.open("index.md", "w") as _md_file, open("scripts/index.md.template") as index_template:
+with mkdocs_gen_files.open("index.md", "w") as _md_file, open("docs/scripts/index.md.template") as index_template:
     _index: list[str] = index_template.readlines()
     for _line in _index:
         _md_file.write(_line.replace("VERSION", CosmoTech_Acceleration_Library.__version__))
