dec0612aa52d08e4f8fa2508235d1d282600f0bd
Laurent Alépée <laurent.alepee@cosmotech.com>
2022-09-05 15:17:21 +0200

    add code references generation in mkdocs
    generate basic code references from source code

---
+++ docs/scripts/generate_references.py
@@ -0,0 +1,59 @@
+import mkdocs_gen_files
+import os
+from pprint import pprint
+from griffe.dataclasses import Kind
+from griffe.dataclasses import Module
+from griffe.dataclasses import Alias
+
+
+pyhand = mkdocs_gen_files.config['plugins']['mkdocstrings'].get_handler('python')
+module_name = 'CosmoTech_Acceleration_Library'
+
+griffed_module = pyhand.collect(module_name, {})
+
+# pprint(griffed_module.as_dict())
+
+
+def yield_module_member(module: Module) -> list:
+    for sub_module in module.modules.values():
+        yield from yield_module_member(sub_module)
+    if len(m_classes := module.classes) > 0:
+        for c in m_classes.values():
+            if isinstance(c, Alias):
+                # escape the classes import which are alias of class
+                continue
+            yield c.path
+    else:
+        if len(module.functions) > 0:
+            yield module.path
+
+
+depth = 1
+parents = {}
+for identifier in yield_module_member(griffed_module):
+    parent, *sub = identifier.rsplit('.', depth)
+    _, parent = parent.split('.', 1)  # remove module name
+    if parent not in parents.keys():
+        parents[parent] = set()
+    parents[parent].add(sub[0])
+
+# gen md files
+with open('docs/scripts/generic_ref.md.template') as f:
+    generic_template_ref = f.read()
+
+mk_nav = mkdocs_gen_files.Nav()
+for nav, file_set in parents.items():
+    nav_root = ['References']
+    nav_root.extend(nav.split('.'))
+    for filz in file_set:
+        file_name = '.'.join([nav, filz, 'md'])
+        nav_file = nav_root[:]
+        if len(file_set) > 1:
+            nav_file.append(filz)
+        mk_nav[nav_file] = file_name
+        with mkdocs_gen_files.open(os.path.join('references', file_name), 'w') as f:
+            f.write(generic_template_ref.replace('%%IDENTIFIER%%', '.'.join([module_name, nav, filz])))
+            f.write('\n')
+
+with mkdocs_gen_files.open("references/SUMMARY.md", "w") as nav_file:
+    nav_file.writelines(mk_nav.build_literate_nav())
