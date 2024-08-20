0040a396b068bcba496f5d29e20730eb9c9a38e0
Alexis Fossart <alexis.fossart@cosmotech.com>
2024-08-20 16:42:25 +0200

    Use csm-orc utils Singleton instead of bad implementation

--- cosmotech/coal/store/store.py
+++ cosmotech/coal/store/store.py
@@ -3,15 +3,10 @@ import pathlib
 
 import pyarrow
 import pyarrow.parquet as pq
+from cosmotech.orchestrator.utils.singleton import Singleton
 
 
-class Store:
-    _instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if not isinstance(cls._instance, cls):
-            cls._instance = object.__new__(cls)
-        return cls._instance
+class Store(metaclass=Singleton):
 
     def __init__(self, reset=False):
         self.store_location = pathlib.Path(os.environ.get("CSM_PARAMETERS_ABSOLUTE_PATH", ".")) / ".coal/store"
@@ -27,7 +22,7 @@ class Store:
     def get_table(self, table_name: str) -> pyarrow.Table:
         if table_name not in self._tables:
             raise FileNotFoundError(f"No table with name {table_name} exists")
-        return pq.read_table(self._tables[table_name]).to_pylist()
+        return pq.read_table(self._tables[table_name])
 
     def add_table(self, table_name: str, data=pyarrow.Table, replace: bool = False) -> pathlib.Path:
         if table_name in self._tables and not replace:
@@ -35,6 +30,7 @@ class Store:
         table_path = self.store_location / f"{table_name}.parquet"
         table_path.parent.mkdir(parents=True, exist_ok=True)
         pq.write_table(data, table_path)
+        self._tables[table_name] = table_path
         return table_path
 
     def list_tables(self) -> list[str]:
