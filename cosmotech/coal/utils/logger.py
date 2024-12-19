5533f9cd51f93795b9c153e34b0478e568b0460f
Alexis Fossart <alexis.fossart@cosmotech.com>
2024-12-19 15:21:11 +0100

    0.9.0 - Move get_logger to csm-orc

--- cosmotech/coal/utils/logger.py
+++ cosmotech/coal/utils/logger.py
@@ -5,45 +5,6 @@
 # etc., to any person is prohibited unless it has been previously and
 # specifically authorized by written means by Cosmo Tech.
 
-import logging
-import os
-
-from rich.console import Console
-from rich.logging import RichHandler
-from rich.highlighter import NullHighlighter
-
-HEADER_LENGTH = 32
-CONTENT_LENGTH = 180
-
-HIGLIGHTER = NullHighlighter()
-HANDLER = RichHandler(rich_tracebacks=True,
-                      omit_repeated_times=False,
-                      show_path=False,
-                      markup=True,
-                      highlighter=HIGLIGHTER,
-                      console=Console(width=HEADER_LENGTH+CONTENT_LENGTH))
-_format = "%(message)s"
-
-if "PAILLETTES" in os.environ:
-    paillettes = "[bold yellow blink]***[/]"
-    _format = f"{paillettes} {_format} {paillettes}"
-
-FORMATTER = logging.Formatter(fmt=_format,
-                              datefmt="[%Y/%m/%d-%H:%M:%S]",
-                              )
-
-HANDLER.setFormatter(FORMATTER)
-
-
-def get_logger(
-    logger_name: str,
-    level=logging.INFO
-) -> logging.Logger:
-    _logger = logging.getLogger(logger_name)
-    if not _logger.hasHandlers():
-        _logger.addHandler(HANDLER)
-    _logger.setLevel(level)
-    return _logger
-
+from cosmotech.orchestrator.utils.logger import get_logger
 
 LOGGER = get_logger("csm.data")
