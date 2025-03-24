95e9b384a8b6822a230fefcd26888b6e75a00719
Alexis Fossart <alexis.fossart@cosmotech.com>
2025-03-24 15:40:52 +0100

    SingleStore Integration: Add SingleStore store functionality with tests

---
+++ cosmotech/coal/singlestore/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (C) - 2023 - 2025 - Cosmo Tech
+# This document and all information contained herein is the exclusive property -
+# including all intellectual property rights pertaining thereto - of Cosmo Tech.
+# Any use, reproduction, translation, broadcasting, transmission, distribution,
+# etc., to any person is prohibited unless it has been previously and
+# specifically authorized by written means by Cosmo Tech.
+
+"""
+SingleStore integration module.
+
+This module provides functions for interacting with SingleStore databases.
+"""
+
+# Re-export functions from the store module
+from cosmotech.coal.singlestore.store import (
+    load_from_singlestore,
+)
