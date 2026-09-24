fe11761fb760b276f9a87a0bbb28a031b7c833a4
Laurent Alépée <laurent.alepee@cosmotech.com>
2026-09-24 14:40:26 +0200

    remove breaking change rollout
    modification will be add on a major version

--- cosmotech/coal/store/output/postgres_channel.py
+++ cosmotech/coal/store/output/postgres_channel.py
@@ -1,12 +1,7 @@
 from typing import Optional
 
-import adbc_driver_manager
-from cosmotech.orchestrator.utils.translate import T
-
 from cosmotech.coal.postgresql.runner import (
     create_metadata,
-    get_metadata_last_run,
-    remove_run_metadata_from_postgresql,
     remove_runner_metadata_from_postgresql,
     send_runner_metadata_to_postgresql,
 )
@@ -16,7 +11,6 @@ from cosmotech.coal.postgresql.store import (
 )
 from cosmotech.coal.store.output.channel_interface import ChannelInterface
 from cosmotech.coal.utils.configuration import Dotdict
-from cosmotech.coal.utils.logger import LOGGER
 
 
 class PostgresChannel(ChannelInterface):
@@ -43,34 +37,22 @@ class PostgresChannel(ChannelInterface):
         if self.configuration.setup_db:
             create_metadata(self.configuration)
 
-        # get old run id (present in metadata table)
-        last_run_ids = get_metadata_last_run(self.configuration)
-        rollout_run_id_list = [run_id for run_id in last_run_ids if run_id != self.configuration.cosmotech.run_id]
-
+        # remove all row for this runner
+        remove_runner_metadata_from_postgresql(self.configuration)
         # add new run id in metadata table
         new_run_id = self.configuration.cosmotech.run_id
         send_runner_metadata_to_postgresql(self.configuration)
 
-        try:
-            # Send store's tables to PSQL
-            dump_store_to_postgresql_from_conf(
-                configuration=self.configuration,
-                selected_tables=filter,
-                fk_id=new_run_id,
-                replace=False,
-            )
+        # Send store's tables to PSQL
+        dump_store_to_postgresql_from_conf(
+            configuration=self.configuration,
+            selected_tables=filter,
+            fk_id=new_run_id,
+            replace=False,
+        )
 
-            if self.configuration.setup_db:
-                add_fk_constraints(self.configuration)
-        except adbc_driver_manager.ProgrammingError as e:
-            # remove newly created id
-            LOGGER.error(T("coal.store.output.postgres_channel.data_send_fail"))
-            remove_run_metadata_from_postgresql(self.configuration, new_run_id)
-            raise e
-        # rollout old run id
-        LOGGER.info(T("coal.store.output.postgres_channel.output_rollout").format(run_id=rollout_run_id_list))
-        for last_run_id in rollout_run_id_list:
-            remove_run_metadata_from_postgresql(self.configuration, last_run_id)
+        if self.configuration.setup_db:
+            add_fk_constraints(self.configuration)
 
     def delete(self):
         # removing metadata will trigger cascade delete on real data
--- tests/unit/coal/test_store/test_output/test_postgres_channel.py
+++ tests/unit/coal/test_store/test_output/test_postgres_channel.py
@@ -75,18 +75,16 @@ class TestPostgresChannel:
         assert "user_password" in PostgresChannel.required_keys["postgres"]
 
     @patch("cosmotech.coal.store.output.postgres_channel.create_metadata")
-    @patch("cosmotech.coal.store.output.postgres_channel.get_metadata_last_run")
-    @patch("cosmotech.coal.store.output.postgres_channel.dump_store_to_postgresql_from_conf")
+    @patch("cosmotech.coal.store.output.postgres_channel.remove_runner_metadata_from_postgresql")
     @patch("cosmotech.coal.store.output.postgres_channel.send_runner_metadata_to_postgresql")
+    @patch("cosmotech.coal.store.output.postgres_channel.dump_store_to_postgresql_from_conf")
     @patch("cosmotech.coal.store.output.postgres_channel.add_fk_constraints")
-    @patch("cosmotech.coal.store.output.postgres_channel.remove_run_metadata_from_postgresql")
     def test_send_no_setup_db(
         self,
-        mock_remove_run,
         mock_add_fk,
-        mock_send_metadata,
         mock_dump,
-        mock_get_metadata_last_run,
+        mock_send_metadata,
+        mock_remove_runner,
         mock_create_metadata,
         base_postgres_config,
     ):
@@ -94,38 +92,33 @@ class TestPostgresChannel:
 
         base_postgres_config.setup_db = False  # Set to False to test behavior when setup_db is False
         channel = PostgresChannel(base_postgres_config)
-        mock_get_metadata_last_run.return_value = ["run789", "run456"]
 
         # Act
         channel.send()
 
         # Assert
         mock_create_metadata.assert_not_called()  # This should be called only if setup_db is True
-        mock_get_metadata_last_run.Assert_called_once()
+        mock_remove_runner.assert_called_once()
         mock_send_metadata.assert_called_once()
         mock_dump.assert_called_once()
         mock_add_fk.assert_not_called()  # This should be called only if setup_db is True
-        mock_remove_run.assert_called_once()
 
     @patch("cosmotech.coal.store.output.postgres_channel.create_metadata")
-    @patch("cosmotech.coal.store.output.postgres_channel.get_metadata_last_run")
-    @patch("cosmotech.coal.store.output.postgres_channel.dump_store_to_postgresql_from_conf")
+    @patch("cosmotech.coal.store.output.postgres_channel.remove_runner_metadata_from_postgresql")
     @patch("cosmotech.coal.store.output.postgres_channel.send_runner_metadata_to_postgresql")
+    @patch("cosmotech.coal.store.output.postgres_channel.dump_store_to_postgresql_from_conf")
     @patch("cosmotech.coal.store.output.postgres_channel.add_fk_constraints")
-    @patch("cosmotech.coal.store.output.postgres_channel.remove_run_metadata_from_postgresql")
     def test_send_without_filter(
         self,
-        mock_remove_run,
         mock_add_fk,
-        mock_send_metadata,
         mock_dump,
-        mock_get_metadata_last_run,
+        mock_send_metadata,
+        mock_remove_runner,
         mock_create_metadata,
         base_postgres_config,
     ):
         """Test sending data without table filter."""
         channel = PostgresChannel(base_postgres_config)
-        mock_get_metadata_last_run.return_value = ["run789", "run456"]
 
         # Act
         channel.send()
@@ -133,11 +126,10 @@ class TestPostgresChannel:
         # Assert
         base_postgres_config.setup_db = True
         mock_create_metadata.assert_called_once()
-        mock_get_metadata_last_run.Assert_called_once()
+        mock_remove_runner.assert_called_once()
         mock_send_metadata.assert_called_once()
         mock_dump.assert_called_once()
         mock_add_fk.assert_called_once()
-        mock_remove_run.assert_called_once()
 
         # Check the arguments passed to dump_store_to_postgresql_from_conf
         call_args = mock_dump.call_args
@@ -145,72 +137,23 @@ class TestPostgresChannel:
         assert call_args.kwargs["selected_tables"] is None
         assert call_args.kwargs["fk_id"] == "run789"
 
-        call_args_remove_run = mock_remove_run.call_args
-        assert call_args_remove_run.args[0] == base_postgres_config
-        assert call_args_remove_run.args[1] == "run456"
-
     @patch("cosmotech.coal.store.output.postgres_channel.create_metadata")
-    @patch("cosmotech.coal.store.output.postgres_channel.get_metadata_last_run")
-    @patch("cosmotech.coal.store.output.postgres_channel.dump_store_to_postgresql_from_conf")
+    @patch("cosmotech.coal.store.output.postgres_channel.remove_runner_metadata_from_postgresql")
     @patch("cosmotech.coal.store.output.postgres_channel.send_runner_metadata_to_postgresql")
-    @patch("cosmotech.coal.store.output.postgres_channel.add_fk_constraints")
-    @patch("cosmotech.coal.store.output.postgres_channel.remove_run_metadata_from_postgresql")
-    def test_send_without_filter(
-        self,
-        mock_remove_run,
-        mock_add_fk,
-        mock_send_metadata,
-        mock_dump,
-        mock_get_metadata_last_run,
-        mock_create_metadata,
-        base_postgres_config,
-    ):
-        """Test sending data without table filter."""
-        channel = PostgresChannel(base_postgres_config)
-        mock_get_metadata_last_run.return_value = ["run789", "run456"]
-
-        # Act
-        channel.send()
-
-        # Assert
-        base_postgres_config.setup_db = True
-        mock_create_metadata.assert_called_once()
-        mock_get_metadata_last_run.Assert_called_once()
-        mock_send_metadata.assert_called_once()
-        mock_dump.assert_called_once()
-        mock_add_fk.assert_called_once()
-        mock_remove_run.assert_called_once()
-
-        # Check the arguments passed to dump_store_to_postgresql_from_conf
-        call_args = mock_dump.call_args
-        assert call_args.kwargs["configuration"] == base_postgres_config
-        assert call_args.kwargs["selected_tables"] is None
-        assert call_args.kwargs["fk_id"] == "run789"
-
-        call_args_remove_run = mock_remove_run.call_args
-        assert call_args_remove_run.args[0] == base_postgres_config
-        assert call_args_remove_run.args[1] == "run456"
-
-    @patch("cosmotech.coal.store.output.postgres_channel.create_metadata")
-    @patch("cosmotech.coal.store.output.postgres_channel.get_metadata_last_run")
     @patch("cosmotech.coal.store.output.postgres_channel.dump_store_to_postgresql_from_conf")
-    @patch("cosmotech.coal.store.output.postgres_channel.send_runner_metadata_to_postgresql")
     @patch("cosmotech.coal.store.output.postgres_channel.add_fk_constraints")
-    @patch("cosmotech.coal.store.output.postgres_channel.remove_run_metadata_from_postgresql")
     def test_send_with_filter(
         self,
-        mock_remove_run,
         mock_add_fk,
-        mock_send_metadata,
         mock_dump,
-        mock_get_metadata_last_run,
+        mock_send_metadata,
+        mock_remove_runner,
         mock_create_metadata,
         base_postgres_config,
     ):
         """Test sending data with table filter."""
         channel = PostgresChannel(base_postgres_config)
         tables_filter = ["table1", "table2", "table3"]
-        mock_get_metadata_last_run.return_value = ["run789", "run456"]
 
         # Act
         channel.send(filter=tables_filter)
@@ -218,21 +161,16 @@ class TestPostgresChannel:
         # Assert
         base_postgres_config.setup_db = True
         mock_create_metadata.assert_called_once()
-        mock_get_metadata_last_run.Assert_called_once()
+        mock_remove_runner.assert_called_once()
         mock_send_metadata.assert_called_once()
         mock_dump.assert_called_once()
         mock_add_fk.assert_called_once()
-        mock_remove_run.assert_called_once()
 
         # Check the arguments passed to dump_store_to_postgresql_from_conf
         call_args = mock_dump.call_args
         assert call_args.kwargs["configuration"] == base_postgres_config
         assert call_args.kwargs["selected_tables"] == ["table1", "table2", "table3"]
         assert call_args.kwargs["fk_id"] == "run789"
-
-        call_args_remove_run = mock_remove_run.call_args
-        assert call_args_remove_run.args[0] == base_postgres_config
-        assert call_args_remove_run.args[1] == "run456"
 
     @patch("cosmotech.coal.store.output.postgres_channel.remove_runner_metadata_from_postgresql")
     def test_delete(self, mock_remove_metadata, base_postgres_config):
